5b627b70b2161b4f195a770f6a7d6e4179a27b5c
Anastasiya Mironova <amona@irrelephant.io>
2022-01-23 00:04:13 +0300

    refactor solution

--- [7 kyu] 16+18=214.py
+++ [7 kyu] 16+18=214.py
@@ -5,6 +5,6 @@ def add(num1, num2):
         while len(list2) != len(list1): list2.insert(0, 0)
     if num1 < num2:
         while len(list2) != len(list1): list1.insert(0, 0)
-    c = [int(x) + int(y) for x, y in zip(list1[::-1], list2[::-1])][::-1]
+    c = [int(x) + int(y) for x, y in zip(list1, list2)]
     e = [str(i) for i in c]
     return int("".join(e))
